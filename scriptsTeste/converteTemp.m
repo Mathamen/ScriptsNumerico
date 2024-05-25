ff49bb2f72504d166d3aee4272bab0e6ab06ab94
Matheus Amendoeira <mathamenrj@gmail.com>
2024-05-24 22:47:35 -0300

    aprendi a comentar, último commit por um tempo

--- scriptsTeste/converteTemp.m
+++ scriptsTeste/converteTemp.m
@@ -1,3 +1,12 @@
+%{
+Esta função é responsável por converter temperaturas de C para F, e vice-versa.
+
+Ela recebe como parâmetro:
+temp= valor da temperatura na escala atual
+iscelcius= booleano que indica se a temperatura está em celcius ou não
+
+
+%}
 function y = converteTemp(temp, iscelcius)
     clc;
     if iscelcius == true
